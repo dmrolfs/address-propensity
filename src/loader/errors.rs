3df6cd3406aee9611774dfd733360bc89110bc7d
Damon Rolfs <drolfs@gmail.com>
2021-10-04 13:12:25 -0700

    moved loader error into separate module

---
+++ src/loader/errors.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, thiserror::Error)]
+#[non_exhaustive]
+pub enum LoaderError {
+    #[error("{0}")]
+    SettingsError(#[from] settings_loader::SettingsError),
+
+    #[error("{0}")]
+    CoreError(#[from] crate::core::CoreError),
+
+    #[error("{0}")]
+    ValidationErrors(#[from] validator::ValidationErrors),
+
+    #[error("{0}")]
+    IOError(#[from] std::io::Error),
+
+    #[error("{0}")]
+    CsvError(#[from] csv::Error),
+
+    #[error("{0}")]
+    RepositoryError(#[from] sqlx::Error),
+
+    #[error("Unrecognized land use type: {0}")]
+    UnrecognizedLandUseType(String),
+
+    #[error("{0}")]
+    LoaderError(#[from] anyhow::Error),
+}
--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -2,31 +2,33 @@ pub mod domain;
 pub mod propensity_loader;
 pub mod property_loader;
 pub mod settings;
+pub mod errors;
 
-#[derive(Debug, thiserror::Error)]
-#[non_exhaustive]
-pub enum LoaderError {
-    #[error("{0}")]
-    SettingsError(#[from] settings_loader::SettingsError),
+use  serde::de::DeserializeOwned;
+use crate::loader::errors::LoaderError;
 
-    #[error("{0}")]
-    CoreError(#[from] crate::core::CoreError),
+pub trait DataSource<D>
+where
+    D: DeserializedOwned,
+{
+    type Path;
+    type Iter: Iterator<Item = D>;
 
-    #[error("{0}")]
-    ValidationErrors(#[from] validator::ValidationErrors),
+    fn size(&self) -> usize;
+    fn into_iter(self) -> Self::Iter;
+}
 
-    #[error("{0}")]
-    IOError(#[from] std::io::Error),
+pub trait DataLoader<S, D>
+where
+    S: DataSource<D>,
+{
+    type Error;
+    type QualityMeasure: Default;
+    type DataStore:
 
-    #[error("{0}")]
-    CsvError(#[from] csv::Error),
+    fn run(path: S::Path) -> Result<(), Self::Error> {
 
-    #[error("{0}")]
-    RepositoryError(#[from] sqlx::Error),
+    }
 
-    #[error("Unrecognized land use type: {0}")]
-    UnrecognizedLandUseType(String),
-
-    #[error("{0}")]
-    LoaderError(#[from] anyhow::Error),
+    fn post_process_quality(&self, quality: Self::QualityMeasure) -> Result<(), Self::Error> { Ok(()) }
 }
