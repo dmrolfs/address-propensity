5d6ddce963cfc6b22edbf68be78af906da1eca3d
Damon Rolfs <drolfs@gmail.com>
2021-10-04 18:07:03 -0700

    removed metered reference

--- src/server/errors.rs
+++ src/server/errors.rs
@@ -1,8 +1,6 @@
 use std::fmt::Debug;
 use thiserror::Error;
-use metered::{metered, ErrorCount};
 
-#[metered::error_count(name = PropensityErrorCount, visibility = pub)]
 #[derive(Debug, Error)]
 #[non_exhaustive]
 pub enum PropensityError {
