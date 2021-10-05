ce4b1bda473765d4b3b74de84e80606af2396a22
Damon Rolfs <drolfs@gmail.com>
2021-10-04 18:08:27 -0700

    removed data loaded branch reference

--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -1,34 +1,34 @@
 pub mod domain;
+pub mod errors;
 pub mod propensity_loader;
 pub mod property_loader;
 pub mod settings;
-pub mod errors;
 
-use  serde::de::DeserializeOwned;
-use crate::loader::errors::LoaderError;
+// use  serde::de::DeserializeOwned;
+// use crate::loader::errors::LoaderError;
 
-pub trait DataSource<D>
-where
-    D: DeserializedOwned,
-{
-    type Path;
-    type Iter: Iterator<Item = D>;
-
-    fn size(&self) -> usize;
-    fn into_iter(self) -> Self::Iter;
-}
-
-pub trait DataLoader<S, D>
-where
-    S: DataSource<D>,
-{
-    type Error;
-    type QualityMeasure: Default;
-    type DataStore:
-
-    fn run(path: S::Path) -> Result<(), Self::Error> {
-
-    }
-
-    fn post_process_quality(&self, quality: Self::QualityMeasure) -> Result<(), Self::Error> { Ok(()) }
-}
+// pub trait DataSource<D>
+// where
+//     D: DeserializedOwned,
+// {
+//     type Path;
+//     type Iter: Iterator<Item = D>;
+//
+//     fn size(&self) -> usize;
+//     fn into_iter(self) -> Self::Iter;
+// }
+//
+// pub trait DataLoader<S, D>
+// where
+//     S: DataSource<D>,
+// {
+//     type Error;
+//     type QualityMeasure: Default;
+//     type DataStore:
+//
+//     fn run(path: S::Path) -> Result<(), Self::Error> {
+//
+//     }
+//
+//     fn post_process_quality(&self, quality: Self::QualityMeasure) -> Result<(), Self::Error> { Ok(()) }
+// }
